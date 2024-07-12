bb365ed41fee3be8c6a4232beaaa14a82a373532
langdon <langdon@fedoraproject.org>
2024-07-12 17:32:18 +0000

    converted _config.yml to a symlink to point at the various flavors; added _config.yml to .gitignore so changing it has to be explicit; fixed most of launch.sh but still can't figure out why you have to be in _site; upgraded jekyll to use a more recent version to deal with disappearing version of nokogiri

--- launch.sh
+++ launch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-bundler install
+# Install missing gems
+bundle check || bundle install
+
+sed --follow-symlinks -i "s/<CODESPACE_NAME>/$CODESPACE_NAME/g" _config.yml
 bundle exec jekyll build
-bundler exec jekyll serve -H 0.0.0.0
+
+cd _site
+
+# Run Jekyll in auto-refresh mode (development mode) listening on all IPs
+bundle exec jekyll serve --host 0.0.0.0 --livereload
